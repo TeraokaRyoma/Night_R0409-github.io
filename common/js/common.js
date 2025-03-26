0fd549776d67e930cee99d46e4a06463341fdb7e
Night_R0409 <105405836+TeraokaRyoma@users.noreply.github.com>
2025-03-27 08:49:16 +0900

    a

--- common/js/common.js
+++ common/js/common.js
@@ -1,7 +1,7 @@
 //ヘッダー読み込み
 function loadHeader() {
     $.ajax({
-        url: "/common/html/header.html", // 相対パスに変更
+        url: "/Night_R0409-github.io/common/html/header.html", // 相対パスに変更
         dataType: "html",
         success: function(data) {
             $("body").prepend(data);
@@ -15,7 +15,7 @@ function loadHeader() {
 //フッター読み込み
 function loadFooter() {
     $.ajax({
-        url: "/common/html/footer.html", // 相対パスに変更
+        url: "/Night_R0409-github.io/common/html/footer.html", // 相対パスに変更
         dataType: "html",
         success: function(data) {
             $("body").append(data);
