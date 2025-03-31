28ec6202d9b498f341c579df571e94e5d29b3426
Night_R0409 <105405836+TeraokaRyoma@users.noreply.github.com>
2025-03-31 11:09:33 +0900

    インストールプロンプト表示

--- index/js/index.js
+++ index/js/index.js
@@ -3,3 +3,27 @@ $(document).ready(function () {
   loadHeader();
   loadFooter();
 });
+
+$(window).on('beforeinstallprompt', function(event) {
+  // インストールプロンプトの表示を遅らせる
+  event.preventDefault();
+
+  // 遅延したイベントをグローバル変数に保存
+  deferredPrompt = event.originalEvent;
+
+  // 画面ロード時にインストールプロンプトを表示
+  deferredPrompt.prompt();
+
+  // ユーザーの選択結果を待機
+  deferredPrompt.userChoice.then(function(choiceResult) {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('ユーザーがアプリのインストールを許可しました。');
+    } else {
+      console.log('ユーザーがアプリのインストールを拒否しました。');
+    }
+
+    // deferredPromptをnullに設定
+    deferredPrompt = null;
+  });
+});
+
