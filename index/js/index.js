a1e7fa053e9dcbcc1cf60f6e3a1fe61a019b4553
Night_R0409 <105405836+TeraokaRyoma@users.noreply.github.com>
2025-03-31 11:20:46 +0900

    インストールプロンプト修正

--- index/js/index.js
+++ index/js/index.js
@@ -1,29 +1,21 @@
-$(document).ready(function () {
-  //ヘッダー、フッター読み込み
+$(document).ready(function() {
   loadHeader();
   loadFooter();
-});
 
-$(window).on('beforeinstallprompt', function(event) {
-  // インストールプロンプトの表示を遅らせる
-  event.preventDefault();
+  $(window).on('beforeinstallprompt', function(event) {
+    event.preventDefault();
+    const deferredPrompt = event.originalEvent;
 
-  // 遅延したイベントをグローバル変数に保存
-  deferredPrompt = event.originalEvent;
-
-  // 画面ロード時にインストールプロンプトを表示
-  deferredPrompt.prompt();
-
-  // ユーザーの選択結果を待機
-  deferredPrompt.userChoice.then(function(choiceResult) {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('ユーザーがアプリのインストールを許可しました。');
-    } else {
-      console.log('ユーザーがアプリのインストールを拒否しました。');
-    }
-
-    // deferredPromptをnullに設定
-    deferredPrompt = null;
+    deferredPrompt.prompt().then(() => {
+      return deferredPrompt.userChoice;
+    }).then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('ユーザーがアプリのインストールを許可しました。');
+      } else {
+        console.log('ユーザーがアプリのインストールを拒否しました。');
+      }
+    }).catch((error) => {
+      console.error('インストールプロンプトでエラーが発生しました:', error);
+    });
   });
 });
-
