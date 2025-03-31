bab424f7fa8b2fcb0e7868e68f21968bbb66655a
Night_R0409 <105405836+TeraokaRyoma@users.noreply.github.com>
2025-03-31 11:21:58 +0900

    Update index.js

--- index/js/index.js
+++ index/js/index.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
   loadHeader();
   loadFooter();
-
-  $(window).on('beforeinstallprompt', function(event) {
-    event.preventDefault();
-    const deferredPrompt = event.originalEvent;
-
-    deferredPrompt.prompt().then(() => {
-      return deferredPrompt.userChoice;
-    }).then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('ユーザーがアプリのインストールを許可しました。');
-      } else {
-        console.log('ユーザーがアプリのインストールを拒否しました。');
-      }
-    }).catch((error) => {
-      console.error('インストールプロンプトでエラーが発生しました:', error);
-    });
-  });
 });
