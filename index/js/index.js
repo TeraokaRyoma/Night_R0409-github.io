465837408bb36c15288a26c82eb8be013fd7a49f
Night_R0409 <105405836+TeraokaRyoma@users.noreply.github.com>
2025-04-07 11:29:09 +0900

    Update index.js

--- index/js/index.js
+++ index/js/index.js
@@ -2,3 +2,11 @@ $(document).ready(function() {
   loadHeader();
   loadFooter();
 });
+let installPrompt = null;
+const installButton = $("#install"); // jQueryオブジェクトとして要素を取得
+
+$(window).on("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttr("hidden"); // jQueryのremoveAttrメソッドを使用
+});
